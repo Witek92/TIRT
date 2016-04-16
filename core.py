d3537e44f2e17b1f4eb223db042dd1e926064123
Zebedeusz <michalgil@vp.pl>
2016-04-16 13:26:51 +0200

    TCP z GUI
    Wysyłanie pliku przez TCP z dodanym GUI (jak na razie zawiesza się podczas odbierania)

---
+++ core.py
@@ -0,0 +1,34 @@
+import sys
+from PyQt4 import QtCore, QtGui
+from audioplay import WavePlay
+from gui1 import Ui_Form
+from tcpsender import TcpSend
+from tcprec import TcpRec
+
+class StartQT4(QtGui.QMainWindow):
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+        QtCore.QObject.connect(self.ui.sendTool,QtCore.SIGNAL("clicked()"), self.sending)
+        QtCore.QObject.connect(self.ui.recTool,QtCore.SIGNAL("clicked()"), self.receiving)
+                         
+    def sending(self):
+        sender = TcpSend()
+        sender.setName(self.ui.FilePath.text())
+        sender.setHost(self.ui.IPsendTool.text())
+        sender.setPort(int(self.ui.PortsendTool.text()))
+        sender.send()
+
+    def receiving(self):
+        recor = TcpRec()
+        #recor.setName(self.ui.FilePath.text())
+        recor.setHost(self.ui.IPrecTool.text())
+        recor.setPort(int(self.ui.PortrecTool.text()))
+        recor.receive()
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    myapp = StartQT4()
+    myapp.show()
+    sys.exit(app.exec_())
